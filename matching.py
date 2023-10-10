08aed0c72ef4e2a448f2bf1090a565df7dc05d0c
mjlarson2 <mjlarson2@cps.edu>
2023-10-10 10:38:36 -0500

    implemented nothing that works

--- matching.py
+++ matching.py
@@ -1,5 +1,13 @@
 from typing import Any, Callable, List, Tuple
+from match import match
+from database import *
+
+def games_by_publisher(src: List[str]):
+    pass
+
+
+
 
 pa_list: List[Tuple[List[str], Callable[[List[str]], List[Any]]]] = [
-    
+    (str.split("What games have % published"), games_by_publisher)
 ]
