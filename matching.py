c9a2a59ec6633c450067090aacc426b8ddf3f727
mjlarson2 <mjlarson2@cps.edu>
2023-10-10 10:38:46 -0500

    implemented nothing that works

--- matching.py
+++ matching.py
@@ -2,12 +2,24 @@ from typing import Any, Callable, List, Tuple
 from match import match
 from database import *
 
-def games_by_publisher(src: List[str]):
-    pass
+# Database doesn't store this sort of publisher information, but these stay until I make one that does work
+# def games_by_publisher(matches: List[str]):
+#     input = matches[0].lower()
+#     input = input.replace(" ", "-")
+#     return get_games("publishers=" + input)
+
+# def publisher_by_game(matches: List[str]):
+#     input = matches[0].lower()
+#     input = input.replace(" ", "-")
+#     results = get_games("search=\"" + input + "\"") #Gets a list of games with the input name
+#     firstResult = get_game(results["results"][0]["id"]) #Gets the 
+#     return firstResult["publisher"] #Gets the publisher of the first result
+    
 
 
 
 
 pa_list: List[Tuple[List[str], Callable[[List[str]], List[Any]]]] = [
-    (str.split("What games have % published"), games_by_publisher)
+    
 ]
+
